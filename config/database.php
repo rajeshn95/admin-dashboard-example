2e6977470dae4119b72048bc568226354a0a2bcd
rajeshn95 <rajesh@betalectic.com>
2025-04-12 21:56:21 +0530

    changes for the databse connection

--- config/database.php
+++ config/database.php
@@ -97,6 +97,21 @@ return [
             'sslmode' => 'prefer',
         ],
 
+        'dvdrental_pgsql' => [
+            'driver' => 'pgsql',
+            'url' => env('DB_URL'),
+            'host' => env('DVDRENTAL_DB_HOST', '127.0.0.1'),
+            'port' => env('DVDRENTAL_DB_PORT', '5432'),
+            'database' => env('DVDRENTAL_DB_DATABASE', 'laravel'),
+            'username' => env('DVDRENTAL_DB_USERNAME', 'root'),
+            'password' => env('DVDRENTAL_DB_PASSWORD', ''),
+            'charset' => env('DB_CHARSET', 'utf8'),
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'search_path' => 'public',
+            'sslmode' => 'prefer',
+        ],
+
         'sqlsrv' => [
             'driver' => 'sqlsrv',
             'url' => env('DB_URL'),
